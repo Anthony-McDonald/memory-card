82a9739392390b73517e40c90cc5ab6cbc6db25f
Anthony McDonald <89093671+Anthony-McDonald@users.noreply.github.com>
2024-03-26 17:53:01 +0000

    header

---
+++ memory-card/src/components/ScoreDisplayer.jsx
@@ -0,0 +1,16 @@
+import  '../styles/ScoreDisplayer.css'
+
+export default function ScoreDisplayer() {
+    return (
+        <>
+        <div className="score-box">
+        <h5 class="score-element">Score: 0</h5>
+
+        <h5 class="score-element">Best Score: 0</h5>
+
+        </div>
+
+        
+        </>
+    )
+}
